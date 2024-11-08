7a9bbce6838e470a9db8c55befcb4a70dc88bc26
Vivek Pathak <coolvivekpathak2004@gmail.com>
2024-11-08 14:18:27 +0530

    server changes

--- index.js
+++ index.js
@@ -13,18 +13,46 @@ const io = new Server(server, {
     cors: true
 });
 
-io.on("connection", (socket) => {
-    console.log("Socket connected.", socket.id);
+// io.on("connection", (socket) => {
+//     console.log("Socket connected.", socket.id);
     
-    socket.on('code', (data) => {
-        socket.broadcast.emit('code', data);
-    });
+//     socket.on('code', (data) => {
+//         socket.broadcast.emit('code', data);
+//     });
 
-    socket.on('disconnect', () => {
-        console.log("Socket disconnected.", socket.id);
-    });
+//     socket.on('disconnect', () => {
+//         console.log("Socket disconnected.", socket.id);
+//     });
+// });
+
+// server.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
+
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on('code-change', ({ roomId, code }) => {
+    socket.to(roomId).emit('code-change', code);
+  });
+
+  socket.on('leave-room', (roomId) => {
+    socket.leave(roomId);
+    console.log(`User ${socket.id} left room ${roomId}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
 });
 
+
 server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
